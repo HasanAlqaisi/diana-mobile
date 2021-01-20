432d61a417c75510c007dd60f03c314c6d8a53de
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2021-01-20 23:36:11 +0300

    Write tests for Task API

---
+++ test/data/data_sources/task/task_remote_source_test.dart
@@ -0,0 +1,327 @@
+import 'dart:convert';
+
+import 'package:diana/core/constants/_constants.dart';
+import 'package:diana/data/data_sources/task/task_remote_source.dart';
+import 'package:diana/data/remote_models/task/task_response.dart';
+import 'package:diana/data/remote_models/task/task_result.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:diana/core/errors/exception.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../fixtures/fixture_reader.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+void main() {
+  MockHttpClient client;
+  TaskRemoteSourceImpl remoteSource;
+  final taskResponse = TaskResponse.fromJson(json.decode(fixture('task.json')));
+  final taskResult =
+      TaskResult.fromJson(json.decode(fixture('task_result.json')));
+  int offset = 0;
+
+  setUp(() {
+    client = MockHttpClient();
+    remoteSource = TaskRemoteSourceImpl(client: client);
+  });
+
+  group('getTasks', () {
+    test('should return [TaskResponse] if response code is 200', () async {
+      when(client.get(
+        '$baseUrl/task/?limit=10&offset=$offset',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      )).thenAnswer((_) async => http.Response(fixture('task.json'), 200));
+
+      final result = await remoteSource.getTasks(offset);
+
+      expect(result, taskResponse);
+    });
+
+    test('should throw UnAuthException if response code is 401', () {
+      when(client.get(
+        '$baseUrl/task/?limit=10&offset=$offset',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 401));
+
+      final result = remoteSource.getTasks;
+
+      expect(result(offset), throwsA(isA<UnAuthException>()));
+    });
+
+    test('should throw UnknownException if response code is not listed above',
+        () {
+      when(client.get(
+        '$baseUrl/task/?limit=10&offset=$offset',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 500));
+
+      final result = remoteSource.getTasks;
+
+      expect(result(offset), throwsA(isA<UnknownException>()));
+    });
+  });
+
+  group('insertTask', () {
+    test('should return [TaskResult] if response code is 201', () async {
+      when(client.post(
+        '$baseUrl/task/',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          "name": "",
+          "note": "",
+          "reminder": "",
+          "deadline": "",
+          "priority": 0,
+          "done": false
+        },
+      )).thenAnswer(
+          (_) async => http.Response(fixture('task_result.json'), 201));
+
+      final result = await remoteSource.insertTask('', '', '', '', 0, false);
+
+      expect(result, taskResult);
+    });
+
+    test('should throw UnAuthException if response code is 401', () {
+      when(client.post(
+        '$baseUrl/task/',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          "name": "",
+          "note": "",
+          "reminder": "",
+          "deadline": "",
+          "priority": 0,
+          "done": false
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 401));
+
+      final result = remoteSource.insertTask;
+
+      expect(result('', '', '', '', 0, false), throwsA(isA<UnAuthException>()));
+    });
+
+    test('should throw FieldsException if response code is 400', () {
+      when(client.post(
+        '$baseUrl/task/',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          "name": "",
+          "note": "",
+          "reminder": "",
+          "deadline": "",
+          "priority": 0,
+          "done": false
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 400));
+
+      final result = remoteSource.insertTask;
+
+      expect(result('', '', '', '', 0, false), throwsA(isA<FieldsException>()));
+    });
+
+    test('should throw UnknownException if response code is not listed above',
+        () {
+      when(client.post(
+        '$baseUrl/task/',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          "name": "",
+          "note": "",
+          "reminder": "",
+          "deadline": "",
+          "priority": 0,
+          "done": false
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 500));
+
+      final result = remoteSource.insertTask;
+
+      expect(
+          result('', '', '', '', 0, false), throwsA(isA<UnknownException>()));
+    });
+  });
+
+  group('editTask', () {
+    test('should return [TaskResult] if response code is 200', () async {
+      when(client.put(
+        '$baseUrl/task//',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          "name": "",
+          "note": "",
+          "reminder": "",
+          "deadline": "",
+          "priority": 0,
+          "done": false
+        },
+      )).thenAnswer(
+          (_) async => http.Response(fixture('task_result.json'), 200));
+
+      final result = await remoteSource.editTask('', '', '', '', '', 0, false);
+
+      expect(result, taskResult);
+    });
+
+    test('should throw UnAuthException if response code is 401', () {
+      when(client.put(
+        '$baseUrl/task//',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          "name": "",
+          "note": "",
+          "reminder": "",
+          "deadline": "",
+          "priority": 0,
+          "done": false
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 401));
+
+      final result = remoteSource.editTask;
+
+      expect(result('', '', '', '', '', 0, false),
+          throwsA(isA<UnAuthException>()));
+    });
+
+    test('should throw NotFoundException if response code is 404', () {
+      when(client.put(
+        '$baseUrl/task//',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          "name": "",
+          "note": "",
+          "reminder": "",
+          "deadline": "",
+          "priority": 0,
+          "done": false
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 404));
+
+      final result = remoteSource.editTask;
+
+      expect(result('', '', '', '', '', 0, false),
+          throwsA(isA<NotFoundException>()));
+    });
+
+    test('should throw FieldsException if response code is 400', () {
+      when(client.put(
+        '$baseUrl/task//',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          "name": "",
+          "note": "",
+          "reminder": "",
+          "deadline": "",
+          "priority": 0,
+          "done": false
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 400));
+
+      final result = remoteSource.editTask;
+
+      expect(result('', '', '', '', '', 0, false),
+          throwsA(isA<FieldsException>()));
+    });
+
+    test('should throw UnknownException if response code is not listed above',
+        () {
+      when(client.put(
+        '$baseUrl/task//',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          "name": "",
+          "note": "",
+          "reminder": "",
+          "deadline": "",
+          "priority": 0,
+          "done": false
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 500));
+
+      final result = remoteSource.editTask;
+
+      expect(result('', '', '', '', '', 0, false),
+          throwsA(isA<UnknownException>()));
+    });
+  });
+
+  group('deleteTask', () {
+    test('should return [true] if response code is 204', () async {
+      when(client.delete(
+        '$baseUrl/task//',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 204));
+
+      final result = await remoteSource.deleteTask('');
+
+      expect(result, true);
+    });
+
+    test('should throw UnAuthException if response code is 401', () {
+      when(client.delete(
+        '$baseUrl/task//',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 401));
+
+      final result = remoteSource.deleteTask;
+
+      expect(result(''), throwsA(isA<UnAuthException>()));
+    });
+
+    test('should throw NotFoundException if response code is 404', () {
+      when(client.delete(
+        '$baseUrl/task//',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 404));
+
+      final result = remoteSource.deleteTask;
+
+      expect(result(''), throwsA(isA<NotFoundException>()));
+    });
+
+    test('should throw UnknownException if response code is not listed above',
+        () {
+      when(client.delete(
+        '$baseUrl/task//',
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      )).thenAnswer((_) async => http.Response(fixture('detail.json'), 500));
+
+      final result = remoteSource.deleteTask;
+
+      expect(result(''), throwsA(isA<UnknownException>()));
+    });
+  });
+}
