1f822b3ff0c8cc5d847580fdd8ea2dd1156bd382
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2021-01-25 20:16:04 +0300

    Create the app database

---
+++ lib/data/database/app_database/app_database.dart
@@ -0,0 +1,34 @@
+import 'package:diana/data/database/models/habit/habit_table.dart';
+import 'package:diana/data/database/models/habit_log/habitlog_table.dart';
+import 'package:diana/data/database/models/subtask/subtask_table.dart';
+import 'package:diana/data/database/models/tag/tag_table.dart';
+import 'package:diana/data/database/models/task/task_table.dart';
+import 'package:diana/data/database/models/tasktag/tasktag_table.dart';
+import 'package:diana/data/database/models/user/user_table.dart';
+import 'package:moor_flutter/moor_flutter.dart';
+
+part 'app_database.g.dart';
+
+@UseMoor(
+  tables: [
+    HabitTable,
+    HabitlogTable,
+    SubTaskTable,
+    TagTable,
+    TaskTable,
+    TaskTagTable,
+    UserTable,
+  ],
+)
+class AppDatabase extends _$AppDatabase {
+  AppDatabase(QueryExecutor q) : super(q);
+
+  @override
+  int get schemaVersion => 1;
+
+  @override
+  MigrationStrategy get migration =>
+      MigrationStrategy(beforeOpen: (details) async {
+        await customStatement('PRAGMA foreign_keys = ON');
+      });
+}
