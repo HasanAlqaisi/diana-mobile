9431f7db106cfea523ca96784ca931f2952dc1d6
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2021-01-27 19:34:12 +0300

    Add day value mapper

---
+++ lib/core/date/date_helper.dart
@@ -0,0 +1,20 @@
+class DateHelper {
+  static int mapToCorrectDayValue(int day) {
+    if (day == 1)
+      return 0;
+    else if (day == 2)
+      return 1;
+    else if (day == 3)
+      return 2;
+    else if (day == 4)
+      return 3;
+    else if (day == 5)
+      return 4;
+    else if (day == 6)
+      return 5;
+    else if (day == 7)
+      return 6;
+    else
+      return null;
+  }
+}
