1034bcfbb357fc0d1cba04442939e27af255b5d7
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2021-01-27 19:36:54 +0300

    Create user queries

---
+++ lib/data/database/models/user/user_dao.dart
@@ -0,0 +1,19 @@
+import 'package:diana/data/database/app_database/app_database.dart';
+import 'package:diana/data/database/models/user/user_table.dart';
+import 'package:diana/data/remote_models/auth/user.dart';
+import 'package:moor_flutter/moor_flutter.dart';
+
+part 'user_dao.g.dart';
+
+@UseDao(tables: [UserTable])
+class UserDao extends DatabaseAccessor<AppDatabase> with _$UserDaoMixin {
+  UserDao(AppDatabase db) : super(db);
+
+  Future<void> insertUser(User user) async {
+    await into(userTable).insertOnConflictUpdate(UserTable.fromUser(user));
+  }
+
+  Future<void> deleteUser(User user) async {
+    await delete(userTable).go();
+  }
+}
