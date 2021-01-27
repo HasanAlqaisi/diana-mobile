10e37d3748bb162b0e837a05fd24971b7407351f
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2021-01-27 19:37:21 +0300

    Create new needed relations

--- lib/data/database/relations/habit_with_habitlogs/habit_with_habitlogs.dart
+++ lib/data/database/relations/habit_with_habitlogs/habit_with_habitlogs.dart
@@ -1,16 +1,19 @@
 import 'package:diana/data/database/app_database/app_database.dart';
 
-///one-to-many relationship
-class HabitWithHabitLogs {
+///one-to-many relationship betwwen habit and logs
+/// one-to-one relationship between habit and days
+class HabitWitLogsWithDays {
   final HabitData habit;
   final List<HabitlogData> habitLogs;
+  final DaysData days;
 
-  HabitWithHabitLogs({this.habit, this.habitLogs});
+  HabitWitLogsWithDays({this.habit, this.habitLogs, this.days});
 
   @override
   String toString() {
     return '''   habit info: ${habit.name}
     habitLogs info: $habitLogs
+    days info: $days
     ''';
   }
 }
---
+++ lib/data/database/relations/tag_with_tasks/tag_with_tasks.dart
@@ -0,0 +1,14 @@
+import 'package:diana/data/database/app_database/app_database.dart';
+
+/// many-to-many relationship - Wrapper class
+class TagWithTasks {
+  final TagData tag;
+  final List<TaskData> tasks;
+
+  TagWithTasks({this.tag, this.tasks});
+
+  @override
+  String toString() {
+    return 'classInfo - name: ${tag.name}, id: ${tag.id}\n studentsInfo: $tasks';
+  }
+}
