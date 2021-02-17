0df77decd818d34fe01e4db3288ce0f792fa7c98
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2021-02-17 17:46:43 +0300

    Make general request helper

--- lib/core/api_helpers/api.dart
+++ lib/core/api_helpers/api.dart
@@ -1,4 +1,14 @@
+import 'package:diana/core/constants/_constants.dart';
+import 'package:diana/core/errors/failure.dart';
+import 'package:diana/domain/usecases/auth/request_token_usecase.dart';
+import 'package:diana/presentation/login/pages/login_screen.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/route_manager.dart';
+import 'package:diana/injection_container.dart' as di;
+
 class API {
+  static RequestTokenUsecase _requestTokenUsecase =
+      di.sl<RequestTokenUsecase>();
   static int offsetExtractor(String url) {
     if (url != null) {
       final uri = Uri.parse(url);
@@ -6,5 +16,29 @@ class API {
       return int.tryParse(offestString);
     }
     return null;
+  }
+
+  static Future<void> doRequest({
+    Function body,
+    void failedBody(Failure fail),
+    Function successBody,
+  }) async {
+    (await body?.call()).fold((fail) async {
+      print("FAIL happen => ${fail.runtimeType}");
+      if (fail is UnAuthFailure) {
+        final requestTokenResult = await _requestTokenUsecase(kRefreshToken);
+        requestTokenResult.fold((requestTokenFail) {
+          print("FAIL2 happen => ${requestTokenFail.runtimeType}");
+          if (requestTokenFail is UnAuthFailure) {
+            Fluttertoast.showToast(msg: 'Session ended');
+            Get.offAllNamed(LoginScreen.route);
+          } else {
+            failedBody?.call(fail);
+          }
+        }, (r) => body?.call());
+      } else {
+        failedBody?.call(fail);
+      }
+    }, (r) => successBody?.call());
   }
 }
